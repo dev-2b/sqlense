ef32d344800d23bada9a1aed3f94356750c4ad54
Tob <tobias_schuermann@yahoo.de>
2026-07-22 15:59:06 +0200

    connection geschrieben und in main eingebunden

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod db;
 fn main() {
     println!("Hello, world!");
+    match db::establish_connection() {
+        Ok(conn) => {
+            // Use the database connection
+            println!("Successfully established database connection.");
+        }
+        Err(e) => {
+            eprintln!("Error establishing database connection: {}", e);
+        }
+    }
 }
